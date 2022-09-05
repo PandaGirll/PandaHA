35b70dd581e21eb0af9f59be53820302a9d664f7
PandaGirll <nikaklimenko@gmail.com>
2022-09-05 13:22:15 +0300

    first initial commit files on 05-09-2022 13:22:15

---
+++ ha_gitpush.sh
@@ -0,0 +1,8 @@
+# Go to /config folder or 
+# Change this to your Home Assistant config folder if it is different
+git add .
+git status
+git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+git push -u origin master
+exit
+
