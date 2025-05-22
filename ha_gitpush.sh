59d3db589d6f3e5a068c2cfbf04e90d559b993e5
root <root@homeassistant.local.hass.io>
2025-05-22 11:15:01 +0300

    config files on 22-05-2025 11:15:01

--- ha_gitpush.sh
+++ ha_gitpush.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Переходим в директорию конфигурации
+cd /config
+
+# Добавляем все файлы в репозиторий с учетом правил .gitignore
 git add .
-git status
+# git status
+
+# Создаем коммит с сообщением, содержащим текущую дату и время
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
+
+# Отправляем изменения в GitHub
 git push origin master
-exit
+
+# exit
 
